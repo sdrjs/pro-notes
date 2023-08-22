f310911f8e865233c6c67fa979db182628c9850c
Sergey RJS <sergeydes@yandex.ru>
2023-08-22 16:48:02 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const burgerMenu = document.querySelector('.menu__burger');
+const menuBody = document.querySelector('.menu__body');
+burgerMenu.onclick = function(e) {
+    menuBody.classList.toggle('_active');
+    burgerMenu.classList.toggle('_active');
+    if (document.body.style.overflow === '') {
+        document.body.style.overflow = 'hidden';
+    } else {
+        document.body.style.overflow = '';
+    }
+}
+
+const navBody = document.querySelector('.nav__body');
+navBody.onclick = function(e) {
+    if (!e.target.classList.contains('nav__link')) return;
+    const title = e.target.getAttribute('href').slice(1);
+    const elem = document.querySelector(`[data-title="${title}"]`);
+    elem.scrollIntoView({block: "start", behavior: "smooth"});
+}
